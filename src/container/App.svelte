aa8f5d006c02bf48b9be775d1a06c99408777fca
juanfer2 <jfvilladiego3@gmail.com>
2020-09-19 08:51:10 -0500

    first commit

---
+++ src/container/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { onMount } from 'svelte';
+</script>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap');
+  :global(body) {
+    background-color: #f2f2f2;
+    color: #2C2B37;
+    font-family: 'Roboto', sans-serif;
+  }
+  .App {
+    display: grid;
+    grid-template-columns: minmax(auto, 1024px);
+    justify-content: center;
+    align-items: center;
+  }
+
+</style>
+
+<div class="App">
+  <h1>
+    Hello world
+
+  </h1>
+</div>
